ea179413c7583272d340e9ff689b67dd7dcdc48e
Dhairya Rana <cloudofdhairya@gmail.com>
2026-03-29 18:32:00 -0600

    Add course name to the page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Dhairya Rana</h1>
-        <p>CICD Pipeline Assignment</p>
+        <p>Course: DevOps / CICD Pipelines</p>
       </header>
     </div>
   );
 }
-
-export default App;
